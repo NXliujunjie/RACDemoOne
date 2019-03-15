b49be134a68230428321e2b5e4832734f9046676
A本人 <liujunjie@liujunjiedemacbook-pro.local>
2019-03-15 15:53:04 +0800

    Initial Commit

---
+++ RACDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RACDemo
+//
+//  Created by liujunjie on 2019/3/15.
+//  Copyright © 2019年 刘俊杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RACDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RACDemo
+//
+//  Created by liujunjie on 2019/3/15.
+//  Copyright © 2019年 刘俊杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
